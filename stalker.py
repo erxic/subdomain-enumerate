d0beef66c9ab542989551ff07fbc56d37253496b
kiraaa99 <akbarhost666@gmail.com>
2023-01-16 15:00:21 +0700

    validasi

--- stalker.py
+++ stalker.py
@@ -74,13 +74,23 @@ def checking_subdomain(sub, domain, output_subdomain):
         quit()
 
 
+def check_superdomain(domain: str):
+    try:
+        dns.resolver.resolve(domain)
+        return True
+    except dns.resolver.NXDOMAIN:
+        return False
+
 # main_function
 while True:
     print_banner(font, slowprint)
 
     # membaca argument dari terminal
     domain = input("masukkan domain: ")
-
+    if not check_superdomain(domain=domain):
+        print(font.RED+f"domain not valid you should check the network or your domain".upper()+font.ECD)
+        time.sleep(3.00)
+        continue
     # untuk menghitung berapa lama enumerate dijalankan
     start = (time.perf_counter_ns())
     thread_pool: list[Thread] = []
